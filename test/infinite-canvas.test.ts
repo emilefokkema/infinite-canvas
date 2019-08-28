8a1fe397a488cd5f258df19b1f2bd266ceb6f3f8
Emile Fokkema <emilefokkema@gmail.com>
2019-08-28 20:40:56 +0200

    start testing the infinite canvas

---
+++ test/infinite-canvas.test.ts
@@ -0,0 +1,33 @@
+import { InfiniteCanvas } from "../src/infinite-canvas"
+import { InfiniteCanvasRenderingContext2D } from "../src/infinite-context/infinite-canvas-rendering-context-2d"
+
+describe("an infinite canvas", () => {
+	let infiniteCanvas: InfiniteCanvas;
+	let canvas: HTMLCanvasElement;
+
+	beforeEach(() => {
+		canvas = undefined;
+		infiniteCanvas = new InfiniteCanvas(canvas);
+	});
+
+	it("should exist", () => {
+		expect(infiniteCanvas).toBeTruthy();
+	});
+
+	describe("that is asked for a context", () => {
+		let context: InfiniteCanvasRenderingContext2D;
+
+		beforeEach(() => {
+			context = infiniteCanvas.getContext();
+		});
+
+		it("should have returned a context", () => {
+			expect(context).toBeTruthy();
+		});
+
+		it("should not return a different context on another call", () => {
+			const resultOfOtherCall: InfiniteCanvasRenderingContext2D = infiniteCanvas.getContext();
+			expect(resultOfOtherCall).toBe(context);
+		});
+	});
+});
--- test/say-hello.test.js
+++
@@ -1,5 +0,0 @@
-import { sayHello } from "../src/say-hello.ts"
-
-test('says hello', () => {
-  expect(sayHello("emile")).toBe("hello emile");
-});
