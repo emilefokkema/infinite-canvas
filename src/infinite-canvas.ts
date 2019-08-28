ebb7a054cb523a733b883aeea677ee661409e582
Emile Fokkema <emilefokkema@gmail.com>
2019-08-28 17:53:04 +0200

    start creating the infinite canvas

---
+++ src/infinite-canvas.ts
@@ -0,0 +1,13 @@
+import { InfiniteCanvasRenderingContext2D } from "./infinite-context/infinite-canvas-rendering-context-2d"
+import { InfiniteContext } from "./infinite-context/infinite-context"
+
+export class InfiniteCanvas{
+	private context: InfiniteCanvasRenderingContext2D;
+	constructor(private readonly canvas: HTMLCanvasElement){}
+	public getContext(): InfiniteCanvasRenderingContext2D{
+		if(!this.context){
+			this.context = new InfiniteContext(this.canvas);
+		}
+		return this.context;
+	}
+}
