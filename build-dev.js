a00e1b6a80f4a81364f82ac5ff79860cbb4db388
Emile Fokkema <emilefokkema@gmail.com>
2019-11-09 19:22:50 +0100

    add a sandbox

--- build-dev.js
+++ build-dev.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-for(let fileName of ["index.html","index.css"]){
+for(let fileName of ["index.html","index.css", "sandbox.html", "sandbox.css"]){
 	fs.copyFile("./devtools/"+fileName, "./dist/"+fileName, (err) => {
 		if (err) throw err;
 	});
