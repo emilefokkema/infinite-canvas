1f118076dfbdec79c74e530dcf116b8c0533fc56
Emile Fokkema <emilefokkema@gmail.com>
2019-11-15 09:18:17 +0100

    add index.html

---
+++ webpack-gh-pages.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/main.js',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ]
+    },
+    output: {
+        filename: 'infinite-canvas.js',
+        path: path.resolve(__dirname, 'scripts'),
+        library: 'InfiniteCanvas'
+    }
+};
