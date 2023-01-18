b6c8c6b0015cccb75ba2295d4082ba66d473423e
Emile Fokkema <emilefokkema@gmail.com>
2023-01-18 20:22:41 +0100

    version bump

--- docs/dev/page/index.js
+++ docs/dev/page/index.js
@@ -2,7 +2,7 @@ import createDepTrees from './dep-trees.js'
 
 console.log('hello from stackblitz page')
 
-const infCanvasUrl = 'https://cdn.jsdelivr.net/npm/ef-infinite-canvas@0.5.7-alpha/dist/infinite-canvas.js';
+const infCanvasUrl = 'https://cdn.jsdelivr.net/npm/ef-infinite-canvas@0.6.0-alpha/dist/infinite-canvas.js';
 
 const stateType = {
     initializing: 0,
@@ -382,7 +382,7 @@ class EmbeddedProject{
         };
         if(!watching){
             projectConfig.dependencies = {
-                "ef-infinite-canvas": "^0.5.6-alpha"
+                "ef-infinite-canvas": "^0.6.0-alpha"
             };
         }
         const openOptions = this.getOpenOptions(type, files);
