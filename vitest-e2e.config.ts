bde6a25e37c15ac0cd22c2b8b3af768048911e4b
Emile Fokkema <emilefokkema@gmail.com>
2024-12-06 22:35:21 +0100

    changes in vite configs

--- vitest-e2e.config.ts
+++ vitest-e2e.config.ts
@@ -1,19 +1,14 @@
-import { fileURLToPath } from 'url'
 import { defineConfig } from 'vitest/config'
 import { addTestCasesMetadataList } from './test-cases/vite-plugins/add-test-cases-metadata-list'
 
 export default defineConfig({
   test: {
-    include: ['test-e2e/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
-    globalSetup: ['./test-e2e/setup.ts'],
+    include: ['e2e/tests/**/*.spec.ts'],
+    globalSetup: ["e2e/setup/index.ts"],
+    setupFiles: ["e2e/tests/test-page/setup.ts"],
     hookTimeout: 120000,
     testTimeout: 120000,
+    reporters: ['dot']
   },
   plugins: [addTestCasesMetadataList()],
-  resolve: {
-    alias: {
-      'test-page-lib': fileURLToPath(new URL('./test-e2e/test-page-lib/api', import.meta.url)),
-      'infinite-canvas': fileURLToPath(new URL('./src/infinite-canvas', import.meta.url)),
-    }
-  }
 })
